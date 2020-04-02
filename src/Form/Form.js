07455e9ae5796fbcaefdef759ba1586939b6f829
Ryan Bahan <ryan@ryanbahan.com>
2020-04-02 09:46:30 -0600

    update controlled form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -12,17 +12,21 @@ class Form extends Component {
     }
   }
 
-  updateForm = (e) => {
+  updateFormString = (e) => {
     this.setState({[e.target.name]: e.target.value})
+  }
+
+  updateFormNumber = (e) => {
+    this.setState({[e.target.name]: parseInt(e.target.value)})
   }
 
   render() {
     return (
       <form>
-      <input type="text" name="name" placeholder="Name" onChange={this.updateForm} required />
-      <input type="text" name="date" placeholder="Date(mm/dd)" onChange={this.updateForm} required />
-      <input type="text" name="time" placeholder="Time" onChange={this.updateForm} required />
-      <input type="number" name="number" placeholder="Number of guests" onChange={this.updateForm} required />
+      <input type="text" name="name" placeholder="Name" onChange={this.updateFormString} required />
+      <input type="text" name="date" placeholder="Date(mm/dd)" onChange={this.updateFormString} required />
+      <input type="text" name="time" placeholder="Time" onChange={this.updateFormString} required />
+      <input type="number" name="number" placeholder="Number of guests" onChange={this.updateFormNumber} required />
       <button>Make Reservation</button>
       </form>
     )
