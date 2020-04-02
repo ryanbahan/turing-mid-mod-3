a0abf35c37f376d687dc1f1cbeac8109ee58f2bc
Ryan Bahan <ryan@ryanbahan.com>
2020-04-02 10:34:06 -0600

    Add form event testing

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -56,18 +56,66 @@ let utils;
 describe("Form event testing", () => {
   let utils;
 
-    beforeEach(() => {
-      utils = render(
-        <Form />
-      )
-    })
+  beforeEach(() => {
+    utils = render(
+      <Form handleSubmit={jest.fn()}/>
+    )
+  })
 
-    afterEach(() => {
-      cleanup
-    })
+  afterEach(() => {
+    cleanup
+  })
 
-    it("Should update name field on user input", () => {
+  it("Should update name field on user input", () => {
+    const { getByPlaceholderText } = utils;
+    const name = getByPlaceholderText("Name");
 
-    })
-    
+    fireEvent.change(name, { target: { value: 'Tommy' } });
+
+    expect(name.value).toBe("Tommy");
+  })
+
+  it("Should update date field on user input", () => {
+    const { getByPlaceholderText } = utils;
+    const date = getByPlaceholderText("Date(mm/dd)");
+
+    fireEvent.change(date, { target: { value: '06/06/06' } });
+
+    expect(date.value).toBe("06/06/06");
+  })
+
+  it("Should update time field on user input", () => {
+    const { getByPlaceholderText } = utils;
+    const time = getByPlaceholderText("Time");
+
+    fireEvent.change(time, { target: { value: '6:66' } });
+
+    expect(time.value).toBe("6:66");
+  })
+
+  it("Should update number of guests field on user input", () => {
+    const { getByPlaceholderText } = utils;
+    const number = getByPlaceholderText("Number of guests");
+
+    fireEvent.change(number, { target: { value: '6' } });
+
+    expect(number.value).toBe("6");
+  })
+
+  it("Should reset form on submission", () => {
+    const { getByText, getByPlaceholderText } = utils;
+    const name = getByPlaceholderText("Name");
+    const date = getByPlaceholderText("Date(mm/dd)");
+    const time = getByPlaceholderText("Time");
+    const number = getByPlaceholderText("Number of guests");
+    const submit = getByText("Make Reservation");
+
+    fireEvent.click(submit);
+
+    expect(name.value).toBe("");
+    expect(date.value).toBe("");
+    expect(time.value).toBe("");
+    expect(number.value).toBe("0");
+  })
+
 })
